8e4aa0a0d9e61ae92797038dd72300342ffcc096
Osiris8 <miganosiris8@gmaaail.com>
2023-06-12 01:05:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navigation from "./components/Navigation";
+import Countries from "./components/Countries";
 
 function App() {
   return (
     <div className="container mx-auto px-4">
       <Navigation />
+      <Countries />
     </div>
   );
 }
