94fd42d00d3dba738f216c324cbe077d69e3b578
Osiris8 <miganosiris8@gmaaail.com>
2023-06-12 01:03:58 +0200

    update Countries.js

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -6,16 +6,13 @@ import Card from "./Card";
 function Countries() {
   const [data, setData] = useState([]);
   const countrieArray = ["Africa", "America", "Asia", "Europe", "Oceania"];
-  const [search, setSearch] = useState("");
   const [selectedContinent, setSelectedContinent] = useState("");
   useEffect(() => {
     axios
       .get("https://restcountries.com/v3.1/all")
       .then((res) => setData(res.data));
   }, []);
-  function handleChange(e) {
-    return setSearch(e.target.data);
-  }
+
   return (
     <div className="container">
       <div className="row p-3">
@@ -27,7 +24,6 @@ function Countries() {
                 <i className="fa fa-search" aria-hidden="true"></i>
               </span>
               <input
-                onChange={(e) => setSearch(e.target.value)}
                 className="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
                 placeholder="Search for country"
               />
